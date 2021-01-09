085246844c0ab225706ca34d3b38f4489e10efe0
Claudio Ramirez <pub.claudio@gmail.com>
2021-01-10 00:33:42 +0100

    add err to splitMultiCertFile for testing

--- main.go
+++ main.go
@@ -19,10 +19,11 @@ func main() {
 	action()
 }
 
-func skimCerts(certs []string) {
-	for _, certFile := range certs {
+func skimCerts(certFiles []string) {
+	for _, certFile := range certFiles {
 		fmt.Printf("Certificate file %s:\n", certFile)
-		for _, cert := range splitMultiCertFile(certFile) {
+		certs, _ := splitMultiCertFile(certFile) // Errors are shown in output
+		for _, cert := range certs {
 			fmt.Printf("Subject:\t%s\n", cert.Subject)
 			if len(cert.DNSNames) > 0 {
 				fmt.Printf("DNS names:\t%s\n", strings.Join(cert.DNSNames, ", "))
@@ -32,7 +33,7 @@ func skimCerts(certs []string) {
 			fmt.Printf("Not before:\t%s\n", cert.NotBefore)
 			fmt.Printf("Not after:\t%s\n", cert.NotAfter)
 			if cert.MaxPathLen > 0 {
-				fmt.Printf("MaxPathLen:\t%s\n", cert.MaxPathLen)
+				fmt.Printf("MaxPathLen:\t%d\n", cert.MaxPathLen)
 			}
 			if len(cert.OCSPServer) > 0 {
 				fmt.Printf("OCSP servers:\t%s\n", strings.Join(cert.OCSPServer, ", "))
@@ -44,20 +45,53 @@ func skimCerts(certs []string) {
 	}
 }
 
-func splitMultiCertFile(certFile string) []*x509.Certificate {
+func verifyCertAndKey(certFile, keyFile string) {
+	fmt.Printf("Certificate file %s and key file %s:\n", certFile, keyFile)
+	_, err := tls.LoadX509KeyPair(certFile, keyFile)
+	if err != nil {
+		fmt.Println(err.Error())
+		return
+	}
+	fmt.Println("certificate and key match")
+}
+
+func verifyChainFromFiles(rootFiles, intermediateFiles []string, certFile string) {
+	var roots, intermediates []*x509.Certificate
+
+	for _, file := range rootFiles {
+		certs, _ := splitMultiCertFile(file) // Errors are shown in output
+		roots = append(roots, certs...)
+	}
+
+	for _, file := range intermediateFiles {
+		certs, _ := splitMultiCertFile(file) // Errors are shown in output
+		intermediates = append(intermediates, certs...)
+	}
+
+	certs, _ := splitMultiCertFile(certFile) // Errors are shown in output
+	if len(certs) != 1 {
+		fmt.Println("error: only a single cert can be verified")
+	}
+
+	verifyChain(roots, intermediates, certs[0])
+}
+
+// Internal
+func splitMultiCertFile(certFile string) ([]*x509.Certificate, error) {
 	var blocks []*pem.Block
 	var certs []*x509.Certificate
 	pemData, err := ioutil.ReadFile(certFile)
 	if err != nil {
 		fmt.Printf("error: %s\n", err)
-		return nil
+		return nil, err
 	}
 
 	for {
 		block, rest := pem.Decode([]byte(pemData))
 		if block == nil {
-			fmt.Printf("error: invalid data in certificate file (%s)\n", err)
-			return nil
+			err = fmt.Errorf("error: invalid data in certificate file (%s)\n", err)
+			fmt.Printf(err.Error())
+			return nil, err
 		}
 		blocks = append(blocks, block)
 		pemData = rest
@@ -67,26 +101,22 @@ func splitMultiCertFile(certFile string) []*x509.Certificate {
 		}
 	}
 
+	var keepErrStrs []string
 	for _, block := range blocks {
 		cert, err := x509.ParseCertificate(block.Bytes)
 		if err != nil {
 			fmt.Printf("error: can not parse certificate (%s)\n", err)
+			keepErrStrs = append(keepErrStrs, err.Error())
 			continue
 		}
 		certs = append(certs, cert)
 	}
 
-	return certs
-}
-
-func verifyCertAndKey(certFile, keyFile string) {
-	fmt.Printf("Certificate file %s and key file %s:\n", certFile, keyFile)
-	_, err := tls.LoadX509KeyPair(certFile, keyFile)
-	if err != nil {
-		fmt.Println(err.Error())
-		return
+	if keepErrStrs == nil {
+		return certs, nil
 	}
-	fmt.Println("certificate and key match")
+	return certs,
+		fmt.Errorf("error: can not parse certificate (%s)\n", strings.Join(keepErrStrs, ", "))
 }
 
 func verifyChain(roots, intermediates []*x509.Certificate, cert *x509.Certificate) bool {
@@ -112,23 +142,4 @@ func verifyChain(roots, intermediates []*x509.Certificate, cert *x509.Certificat
 
 	fmt.Println("certificate is valid for the supplied chain")
 	return true
-}
-
-func verifyChainFromFiles(rootFiles, intermediateFiles []string, certFile string) {
-	var roots, intermediates []*x509.Certificate
-
-	for _, file := range rootFiles {
-		roots = append(roots, splitMultiCertFile(file)...)
-	}
-
-	for _, file := range intermediateFiles {
-		intermediates = append(intermediates, splitMultiCertFile(file)...)
-	}
-
-	certs := splitMultiCertFile(certFile)
-	if len(certs) != 1 {
-		fmt.Println("error: only a single cert can be verified")
-	}
-
-	verifyChain(roots, intermediates, certs[0])
 }
