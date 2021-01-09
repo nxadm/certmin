64983fea0eca8bc637160d567396d1eda8905463
Claudio Ramirez <pub.claudio@gmail.com>
2021-01-10 00:57:17 +0100

    add tests

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.con/nxadm/certmin
 
 go 1.15
 
-require github.com/spf13/pflag v1.0.5
+require (
+	github.com/spf13/pflag v1.0.5
+	github.com/stretchr/testify v1.6.1
+)
---
+++ main_test.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"strings"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+var (
+	testSerials = []string{
+		"1",
+		"76359301477803385872276235234032301461",
+		"290123421899608141648701916708796095456",
+	}
+)
+
+func TestSplitMultiCertFile(t *testing.T) {
+	certs, err := splitMultiCertFile("t/chain.crt")
+	assert.NoError(t, err)
+	for idx, serial := range testSerials {
+		assert.Equal(t, serial, certs[idx].SerialNumber.String())
+	}
+
+	_, err = splitMultiCertFile("/dev/null")
+	assert.Error(t, err)
+	_, err = splitMultiCertFile(strings.Join(testSerials, ""))
+	assert.Error(t, err)
+	_, err = splitMultiCertFile("t/empty.crt")
+	assert.Error(t, err)
+}
