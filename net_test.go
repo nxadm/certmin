e97092c51aa34ff72e8d8c3eb2ea81315e320c23
Claudio Ramirez <pub.claudio@gmail.com>
2021-01-12 12:01:14 +0100

    add netw functionality

---
+++ net_test.go
@@ -0,0 +1,17 @@
+package main
+
+//
+//import (
+//	"testing"
+//
+//	"github.com/stretchr/testify/assert"
+//)
+//
+//func TestRetrieveCerts(t *testing.T) {
+//	_, err := retrieveCerts("foo", "faa")
+//	assert.Error(t, err)
+//
+//	certs, err := retrieveCerts("tcp", "www.kuleuven.be:443")
+//	assert.NoError(t, err)
+//	assert.True(t, len(certs) > 0)
+//}
