6fe24116eb0ee79dc8e59e72e3a8b51251231fc7
Claudio Ramirez <pub.claudio@gmail.com>
2021-01-19 00:13:56 +0100

    retrieve ssl certs code

--- net.go
+++ net.go
@@ -1,37 +1,53 @@
 package certmin
 
-//
-//import (
-//	"crypto/tls"
-//	"crypto/x509"
-//	"errors"
-//	"fmt"
-//	"net"
-//	"net/url"
-//	"os"
-//	"strconv"
-//
-//	"github.com/fatih/color"
-//)
-//
+import (
+	"crypto/tls"
+	"crypto/x509"
+	"errors"
+	"net"
+	"regexp"
+	"time"
+)
 
-//func retrieveCerts(addr string) ([]*x509.Certificate, error) {
-//	var conn *tls.Conn
-//	var err1, err2 error
-//	conn, err1 = tls.Dial("tcp", addr, nil)
-//	if err1 != nil {
-//		conn, err2 = tls.Dial("tcp", addr, &tls.Config{InsecureSkipVerify: true})
-//		if err2 != nil {
-//			return nil, err2
-//		}
-//		fmt.Fprintf(os.Stderr, color.YellowString("\nWARNING: %s\n"), err1)
-//	}
-//	defer conn.Close()
-//
-//	if len(conn.ConnectionState().PeerCertificates) == 0 {
-//		err := errors.New("no certificates found")
-//		return nil, err
-//	}
-//
-//	return conn.ConnectionState().PeerCertificates, nil
-//}
+// RetrieveRemoteCerts retrieves all the certificates offered by the remote host. As parameters
+// it takes an address string in the form of hostname:port and a time-out duration for the
+// connection. The time-out is used for both the TCP and the SSL connection, with 0 disabling it.
+// The return values are an array of certificates (with the first element being the certificate
+// of the server), an error with a warning (mismatch between the hostname and the CN or DNS alias
+// in the certificate) and an error in case of failure.
+func RetrieveRemoteCerts(addr string, timeOut time.Duration) ([]*x509.Certificate, error, error) {
+	conn, err := net.DialTimeout("tcp", addr, timeOut)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	var warning error
+	rx := regexp.MustCompile(":\\d+$")
+	tlsConn := tls.Client(conn, &tls.Config{ServerName: rx.ReplaceAllString(addr, "")})
+	err = tlsConn.SetDeadline(time.Now().Add(timeOut))
+	if err != nil {
+		return nil, nil, err
+	}
+	err = tlsConn.Handshake()
+	if err != nil {
+		tlsConn = tls.Client(conn, &tls.Config{InsecureSkipVerify: true})
+		err2 := tlsConn.SetDeadline(time.Now().Add(timeOut))
+		if err2 != nil {
+			return nil, nil, err2
+		}
+		err2 = tlsConn.Handshake()
+		if err2 != nil {
+			return nil, nil, err2
+		}
+		warning = err
+	}
+	defer tlsConn.Close()
+	defer conn.Close()
+
+	if len(tlsConn.ConnectionState().PeerCertificates) == 0 {
+		err := errors.New("no certificates found")
+		return nil, warning, err
+	}
+
+	return tlsConn.ConnectionState().PeerCertificates, warning, nil
+}
--- net_test.go
+++ net_test.go
@@ -1,21 +1,33 @@
 package certmin
 
-//
-//import (
-//	"os"
-//	"testing"
-//
-//	"github.com/stretchr/testify/assert"
-//)
-//
-//func TestRetrieveCerts(t *testing.T) {
-//	certs, err := retrieveCerts("faa")
-//	assert.Nil(t, certs)
-//	assert.Error(t, err)
-//	if os.Getenv("AUTHOR_TESTING") != "" {
-//		certs, err = retrieveCerts("github.com:443")
-//		assert.NoError(t, err)
-//		assert.True(t, len(certs) >= 2)
-//	}
-//}
-//
+import (
+	"os"
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestRetrieveRemoteCerts(t *testing.T) {
+	certs, warn, err := RetrieveRemoteCerts("faa", 1*time.Second)
+	assert.Nil(t, certs)
+	assert.NoError(t, warn)
+	assert.Error(t, err)
+
+	certs, warn, err = RetrieveRemoteCerts("faa", 0)
+	assert.Nil(t, certs)
+	assert.NoError(t, warn)
+	assert.Error(t, err)
+
+	if os.Getenv("AUTHOR_TESTING") != "" {
+		certs, warn, err = RetrieveRemoteCerts("github.com:443", 5*time.Second)
+		assert.NoError(t, warn)
+		assert.NoError(t, err)
+		assert.True(t, len(certs) >= 2)
+
+		certs, warn, err = RetrieveRemoteCerts("8.8.8.8:443", 5*time.Second)
+		assert.NoError(t, warn)
+		assert.NoError(t, err)
+		assert.True(t, len(certs) >= 2)
+	}
+}
