a37b1959056b1eea971b2d86514062f5e0b91255
Claudio Ramirez <pub.claudio@gmail.com>
2021-01-19 19:48:18 +0100

    debug follow issuer url

--- net.go
+++ net.go
@@ -62,60 +62,13 @@ func RetrieveCertsFromAddr(addr string, timeOut time.Duration) ([]*x509.Certific
 // 0 disabling it. The return values are a []*x509.Certificate (with the first element
 // being the supplied certificate) and an error in case of failure.
 func RetrieveChainFromIssuerURLs(cert *x509.Certificate, timeOut time.Duration) ([]*x509.Certificate, error) {
-	//RecursiveHopCerts(
-	//	cert *x509.Certificate, chain *[]*x509.Certificate, err *error, timeOut time.Duration) *x509.Certificate
 	var chain  []*x509.Certificate
 	var lastErr error
 	recursiveHopCerts(cert, &chain, &lastErr, timeOut)
 	return chain, lastErr
-
-	//if cert == nil || len(cert.IssuingCertificateURL) == 0 {
-	//	return nil, errors.New("no Issuing Certificate URLs")
-	//}
-	//
-	//var retrievedChain []*x509.Certificate
-	//client := http.Client{Timeout: timeOut}
-	//var lastErr error
-	//certToCheck := cert
-	//
-	//OUTER:
-	//for {
-	//	fmt.Printf("Checking and appending CERT: %s\n", cert.Subject)
-	//	retrievedChain = append(retrievedChain, certToCheck)
-	//	INNER:
-	//	for _, url := range certToCheck.IssuingCertificateURL {
-	//		resp, err := client.Get(url)
-	//		if err != nil {
-	//			lastErr = err
-	//			continue
-	//		}
-	//
-	//		bodyBytes, err := ioutil.ReadAll(resp.Body)
-	//		if err != nil {
-	//			lastErr = err
-	//			continue
-	//		}
-	//		defer resp.Body.Close()
-	//
-	//		decodedCerts, err := DecodeCertBytes(bodyBytes)
-	//		if err != nil {
-	//			lastErr = err
-	//			continue
-	//		}
-	//
-	//		fmt.Printf("Next CERT: %s\n", decodedCerts[0].Subject)
-	//		retrievedChain = append(retrievedChain, decodedCerts[0])
-	//		certToCheck = decodedCerts[0]
-	//		lastErr = nil
-	//		break INNER
-	//	}
-	//
-	//	break OUTER
-	//}
-	//
-	//return retrievedChain, lastErr
 }
 
+// recursiveHopCerts follows the URL links recursively
 func recursiveHopCerts(
 	cert *x509.Certificate, chain *[]*x509.Certificate, lastErr *error, timeOut time.Duration) *x509.Certificate {
 	if cert == nil || len(cert.IssuingCertificateURL) == 0 {
@@ -123,6 +76,7 @@ func recursiveHopCerts(
 	}
 
 	fmt.Printf("Checking and appending CERT: %s\n", cert.Subject)
+	fmt.Printf("Recursive URLs: %#v\n", cert.IssuingCertificateURL)
 
 	client := http.Client{Timeout: timeOut}
 	*chain = append(*chain, cert)
